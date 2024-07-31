01188d5144b10f48e31794c4f370dfbcca03c736
Daniel Moreno <foliodox@gmail.com>
2024-07-30 21:23:21 -0500

    Start window update

--- Trixi.V2/App.xaml.cs
+++ Trixi.V2/App.xaml.cs
@@ -9,6 +9,11 @@ namespace Trixi.V2
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            StartWindow startWindow = new StartWindow();
+            startWindow.Show();
+        }
     }
 
 }
---
+++ Trixi.V2/StartWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Trixi.V2
+{
+    /// <summary>
+    /// Lógica de interacción para StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
